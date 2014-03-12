26a860c5cd85af9412b8a72d13e6cb708bddc35a
Jan-Philip Loos <maxdaten@gmail.com>
2014-03-12 03:08:45 +0100

    added At/Idxed instances for Data.Trie

--- src/Yage/Lens.hs
+++ src/Yage/Lens.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE LiberalTypeSynonyms #-}
+{-# LANGUAGE TypeFamilies #-}
 module Yage.Lens (
       module Yage.Lens
     , module Lens
     ) where
 
-import qualified   Prelude
+import Prelude (id, const)
 import Data.Maybe
+import Data.Trie as Trie
+import Data.ByteString
+
+import Control.Applicative (pure)
 
 import Control.Lens as Lens
 
@@ -13,6 +17,23 @@ infixr 4 %?~
 
 
 -- | TODO a maybe modify operator
-(%?~) :: Simple ASetter s a -> Maybe a -> s -> s
-l %?~ mf = over l (\a -> maybe a Prelude.id mf)
+(%?~) :: ASetter' s a -> Maybe a -> s -> s
+l %?~ mf = over l (\a -> maybe a id mf)
 {-# INLINE (%?~) #-}
+
+type instance Index (Trie a) = ByteString
+
+type instance IxValue (Trie a) = a
+instance Ixed (Trie a) where
+  ix k f m = case Trie.lookup k m of
+     Just v -> f v <&> \v' -> Trie.insert k v' m
+     Nothing -> pure m
+  {-# INLINE ix #-}
+
+
+instance At (Trie a) where
+  at k f m = f mv <&> \r -> case r of
+    Nothing -> maybe m (const (Trie.delete k m)) mv
+    Just v' -> Trie.insert k v' m
+    where mv = Trie.lookup k m
+  {-# INLINE at #-}
--- src/Yage/Text.hs
+++ src/Yage/Text.hs
@@ -1,5 +1,7 @@
 module Yage.Text 
     ( module Data.Text.Format
+    , module Data.Text.Lazy
     ) where
 
 import Data.Text.Format
+import Data.Text.Lazy
