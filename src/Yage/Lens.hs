63700d325340c6c23ed5aa0ab1938182f1641d9d
Jan-Philip Loos <maxdaten@gmail.com>
2014-08-07 18:20:26 +0200

    clean up

--- src/Yage/Lens.hs
+++ src/Yage/Lens.hs
@@ -6,6 +6,7 @@
 module Yage.Lens (
       module Yage.Lens
     , module Lens
+    , module BSLens
     ) where
 
 import Prelude (id, const, flip)
@@ -15,6 +16,8 @@ import Control.Arrow hiding ((<+>))
 import Data.Trie as Trie
 import Data.Vinyl
 import Data.ByteString
+import Data.ByteString.Lens as BSLens
+
 import Control.Monad.State (execState, State)
 
 import Control.Applicative (pure)
@@ -61,7 +64,7 @@ update = flip execState
 -- http://www.reddit.com/r/haskell/comments/1nwetz/lenses_that_work_with_arrows/cd2w5f1
 -- https://gist.github.com/fizbin/7217274
 (<~~) :: (Arrow ar) => ASetter s t a b -> ar s b -> ar s t
-setter <~~ arrval = 
+setter <~~ arrval =
   proc x -> do
     bval <- arrval -< x
     returnA -< x & setter .~ bval
